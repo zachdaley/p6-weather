ace46d05832916468d21de2626b7106282bcb526
Zach <zachary.daley@eagles.ewu.edu>
2014-03-16 22:33:08 -0700

    Deciding what to keep

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,13 @@ $(document).ready(function(){
       
       // Get & Store Weather Data
       // html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
-      var temp, tomHi, tomLo, cityAndState; 
+      var spokanetemp, spokanetomHi, spokanetomLo, spokanecityAndState; 
 
       spokanetemp = weather.temp + '<span> f</span>';
       spokanetomHi = weather.tomorrow.high;
       spokanetomLo = weather.tomorrow.low;
       spokanecityAndState = weather.city + ' , ' + weather.region;
 
-      console.log(cityAndState);
 
       // Display Weather
       $('.spokanetemp').html(spokanetemp);
@@ -35,6 +34,65 @@ $(document).ready(function(){
   
   });
 
-	console.log('Page Loaded. Lets Do this!');
+    $.simpleWeather({
 
+     location: '98101',
+    
+    success: function(weather) {
+      
+      // Get & Store Weather Data
+      // html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
+      var seattletemp, seattletomHi, seattletomLo, seattlecityAndState; 
+
+      seattletemp = weather.temp + '<span> f</span>';
+      seattletomHi = weather.tomorrow.high;
+      seattletomLo = weather.tomorrow.low;
+      seattlecityAndState = weather.city + ' , ' + weather.region;
+
+
+      // Display Weather
+      $('.seattletemp').html(seattletemp);
+      $('.seattletomHi').html(seattletomHi);
+      $('.seattletomLo').html(seattletomLo);
+      $('.seattlecityAndState').html(seattlecityAndState);
+
+    },
+
+     error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+
+    });
+
+     $.simpleWeather({
+
+     location: '98902',
+    
+    success: function(weather) {
+      
+      // Get & Store Weather Data
+      // html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
+      var yakimatemp, yakimatomHi, yakimatomLo, yakimacityAndState; 
+
+      yakimatemp = weather.temp + '<span> f</span>';
+      yakimatomHi = weather.tomorrow.high;
+      yakimatomLo = weather.tomorrow.low;
+     yakimacityAndState = weather.city + ' , ' + weather.region;
+
+
+      // Display Weather
+      $('.yakimatemp').html(yakimatemp);
+      $('.yakimatomHi').html(yakimatomHi);
+      $('.yakimatomLo').html(yakimatomLo);
+      $('.yakimacityAndState').html(yakimacityAndState);
+
+    },
+
+     error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+
+    });
 }); 
+
+
