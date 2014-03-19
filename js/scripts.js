bb6b517cc18c5206a7a8d4847ad3fbcbc06869a7
Zach <zachary.daley@eagles.ewu.edu>
2014-03-18 19:14:30 -0700

    Scrapped ideas and added zipcode feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,59 @@
-// Make Foundation Go!
+// Run Foundation
 $(document).foundation();
 
-// Your Awesome Scripts!
-$(document).ready(function(){
+//Zipcode Retrieval
 
-	$.simpleWeather({
+  $('button').on('click', function() {
+  
+
+  var zipcode = $('#getWeather').val();
+  
+
+  $.simpleWeather({
     
-    location: '99206',
-    
+    location: zipcode,
+  
     success: function(weather) {
       
-      // Get & Store Weather Data
-      // html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
-      var spokanetemp, spokanetomHi, spokanetomLo, spokanecityAndState; 
 
-      spokanetemp = weather.temp + '<span> f</span>';
-      spokanetomHi = weather.tomorrow.high;
-      spokanetomLo = weather.tomorrow.low;
-      spokanecityAndState = weather.city + ' , ' + weather.region;
+      var temp = weather.temp;
+
+      var city = weather.city;
+
+      var currently = weather.currently;
+
+      var code = '<img src=' + weather.image + '>';
+
+      var temp = weather.temp + '&deg;' + weather.units.temp;
+
+      var low = 'Low: ' + weather.low + '&deg' + weather.units.temp;
+      
+      var high = 'High: ' + weather.high + '&deg' + weather.units.temp;
+ 
+
+      // Output to hooks in HTML
+      $('.code').html(code);
+      $('.temp').html(temp);
+      $('.city').html(city);
+      $('.currently').html(currently);
+      $('.low').html(low);
+      $('.high').html(high);
 
 
-      // Display Weather
-      $('.spokanetemp').html(spokanetemp);
-      $('.spokanetomHi').html(spokanetomHi);
-      $('.spokanetomLo').html(spokanetomLo);
-      $('.spokanecityAndState').html(spokanecityAndState);
 
+      console.log(weather);
     },
-    
+  
     error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
+      $('body').html('<p>' + error + '</p>');
     }
   
   });
-
-    $.simpleWeather({
-
-     location: '98101',
-    
-    success: function(weather) {
-      
-      // Get & Store Weather Data
-      // html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
-      var seattletemp, seattletomHi, seattletomLo, seattlecityAndState; 
-
-      seattletemp = weather.temp + '<span> f</span>';
-      seattletomHi = weather.tomorrow.high;
-      seattletomLo = weather.tomorrow.low;
-      seattlecityAndState = weather.city + ' , ' + weather.region;
+  
+  $('#getWeather').val('');
+  
+});
 
 
-      // Display Weather
-      $('.seattletemp').html(seattletemp);
-      $('.seattletomHi').html(seattletomHi);
-      $('.seattletomLo').html(seattletomLo);
-      $('.seattlecityAndState').html(seattlecityAndState);
-
-    },
-
-     error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-
-    });
-
-     $.simpleWeather({
-
-     location: '98902',
-    
-    success: function(weather) {
-      
-      // Get & Store Weather Data
-      // html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
-      var yakimatemp, yakimatomHi, yakimatomLo, yakimacityAndState; 
-
-      yakimatemp = weather.temp + '<span> f</span>';
-      yakimatomHi = weather.tomorrow.high;
-      yakimatomLo = weather.tomorrow.low;
-     yakimacityAndState = weather.city + ' , ' + weather.region;
-
-
-      // Display Weather
-      $('.yakimatemp').html(yakimatemp);
-      $('.yakimatomHi').html(yakimatomHi);
-      $('.yakimatomLo').html(yakimatomLo);
-      $('.yakimacityAndState').html(yakimacityAndState);
-
-    },
-
-     error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-
-    });
-}); 
 
 
